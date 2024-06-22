474ad78d81877302c708fe69a338964c12cd365a
ASUS <dharmeshgadhiya161@gmail.com>
2024-06-22 19:35:56 +0530

    ProductItemApp

--- app/src/main/java/com/sunil/dhwarehouse/MainActivity.kt
+++ app/src/main/java/com/sunil/dhwarehouse/MainActivity.kt
@@ -43,11 +43,13 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var sharedPrefManager: SharedPrefManager
-    private lateinit var aryAccount1: MutableList<AccountMaster>
-    lateinit var showingDialog : ShowingDialog
-    lateinit var  accountDataAdapter: AccountDataAdapter
+    lateinit var aryAccount1: MutableList<AccountMaster>
+    private lateinit var showingDialog: ShowingDialog
+    private lateinit var accountDataAdapter: AccountDataAdapter
+
+    lateinit var dayName: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -64,340 +66,28 @@ class MainActivity : AppCompatActivity() {
         sharedPrefManager = SharedPrefManager(this@MainActivity)
         aryAccount1 = ArrayList()
 
-//        if (!sharedPrefManager.isExcelFileShowing) {
-//            showAddExcelFileDialog(dialog)
-//        } else {
-            lifecycleScope.launch(Dispatchers.IO) {
-                val accountDao = MasterDatabase.getDatabase(this@MainActivity).accountDao()
-                aryAccount1 = accountDao.getAccountMaster()
+        lifecycleScope.launch(Dispatchers.IO) {
+            val accountDao = MasterDatabase.getDatabase(this@MainActivity).accountDao()
+            aryAccount1 = accountDao.getAccountMaster()
 
-                withContext(Dispatchers.Main) {
-//                    for (item in aryAccount1) {
-//
-//                        Log.e("tag", "onCreate: ${item.account_name}")
-//                    }
+            withContext(Dispatchers.Main) {
+                accountDataAdapter = AccountDataAdapter(aryAccount1)
+                binding.rvAllAccount.layoutManager = LinearLayoutManager(this@MainActivity)
+                binding.rvAllAccount.adapter = accountDataAdapter
 
-                    accountDataAdapter = AccountDataAdapter(aryAccount1)
-                    binding.rvAllAccount.layoutManager = LinearLayoutManager(this@MainActivity)
-                    binding.rvAllAccount.adapter = accountDataAdapter
+                Log.e("tag", "onCreate: ${aryAccount1.size}")
 
-                    Log.e("tag", "onCreate: ${aryAccount1.size}")
-
-                }
             }
-//        }
-
-
-//        binding.tvSelectAccountMaster.setOnClickListener {
-//            fileType = 1
-//            if (checkPermission()) {
-//                openFilePicker()
-//            } else {
-//                requestPermission()
-//            }
-//        }
-//
-//        binding.tvSelectItemMaster.setOnClickListener {
-//            fileType = 2
-//            if (checkPermission()) {
-//                openFilePicker()
-//            } else {
-//                requestPermission()
-//            }
-//        }
-
+        }
     }
 
-//    private fun checkPermission(): Boolean {
-//        return ContextCompat.checkSelfPermission(
-//            this,
-//            android.Manifest.permission.READ_EXTERNAL_STORAGE
-//        ) == PackageManager.PERMISSION_GRANTED
-//    }
-//
-//    private fun requestPermission() {
-//        ActivityCompat.requestPermissions(
-//            this,
-//            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), storage_permission_code
-//        )
-//    }
-//
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<out String>, grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == storage_permission_code) {
-//            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-//                // Permission granted, proceed with file selection
-//                openFilePicker()
-//            } else {
-//                // Permission denied
-//                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//    }
-//
-//    private fun openFilePicker() {
-//        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-//            this.addCategory(Intent.CATEGORY_OPENABLE)
-//            this.type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-//        }
-//        startActivityForResult(intent, fileRequestCode)
-//    }
-//
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//
-//        Log.d("Excel Data", "Come Result")
-//        if (requestCode == fileRequestCode && resultCode == Activity.RESULT_OK) {
-//            Log.d("Excel Data", "Come Result ok")
-//            data?.data?.also { uri ->
-//                Log.d("Excel Data", "Start")
-//
-//                CoroutineScope(Dispatchers.IO).launch {
-//
-//                    if (fileType == 1) {
-//
-//                        val data = readExcelAccountMasterFile(uri)
-//                        val accountDao = MasterDatabase.getDatabase(this@MainActivity).accountDao()
-//
-//                        accountDao.deleteAllAccounts()
-//                        data?.forEach { row ->
-//                            accountDao.insert(row)
-//
-//                        }
-//                        aryAccount1 = accountDao.getAccountMaster()
-//                        Log.d("Master RoomDB", "Account Master come to add : " + data.size)
-//                        Log.d("Master RoomDB aryAccount1", "Account Master come to add aryAccount1 : " + aryAccount1.size)
-//                        sharedPrefManager.isAccountExcelRead = true
-//                    } else {
-//                        showDialog(this@MainActivity)
-//                        var itemDao = MasterDatabase.getDatabase(this@MainActivity).itemDao()
-//                        val data = readExcelItemMasterFile(uri)
-//                        Log.d("Master RoomDB", "Item Master come to add : " + data.size)
-//                        itemDao.deleteAllItems()
-//                        data.forEach { row ->
-//                            itemDao.insert(row)
-//                        }
-//                        sharedPrefManager.isItemExcelRead = true
-//
-//                        if (dialog.isShowing) {
-//                            dialog.dismiss()
-//                            sharedPrefManager.isExcelFileShowing = true
-//
-//                            dismissDialog(this@MainActivity)
-//
-//                        }
-//                        Log.d("Master RoomDB", "Item Master data added.")
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    suspend fun readExcelAccountMasterFile(uri: Uri): ArrayList<AccountMaster> {
-//        return withContext(Dispatchers.IO) {
-//            Log.d("Excel Data", "Account Master Start1")
-//
-//            var aryAccount: ArrayList<AccountMaster> = ArrayList<AccountMaster>()
-//
-//            val inputSteam = contentResolver.openInputStream(uri)
-//            val workbook = XSSFWorkbook(inputSteam)
-//            //val workbook = WorkbookFactory.create(inputSteam)
-//            val sheet = workbook.getSheetAt(0)
-//            val iterator = sheet.iterator()
-//
-//            var isFirst: Boolean = true
-//            while (iterator.hasNext()) {
-//
-//                var accountMaster: AccountMaster = AccountMaster()
-//                val row = iterator.next()
-//                val cellIterator = row.cellIterator()
-//
-//                if (isFirst) {
-//                    isFirst = false
-//                } else {
-//
-//                    var index: Int = 0
-//                    while (cellIterator.hasNext()) {
-//                        val cell = cellIterator.next()
-//
-//                        if (cell.cellType != CellType.BLANK && cell.cellType != CellType.FORMULA) {
-//
-//                            if (index == 0)
-//                                accountMaster.sales_man = cell.stringCellValue
-//                            else if (index == 1) {
-//                                if (cell.cellType == CellType.STRING) {
-//                                    accountMaster.account_name = cell.stringCellValue
-//                                }
-//                            } else if (index == 2)
-//                                accountMaster.addess = cell.stringCellValue
-//                            else if (index == 3)
-//                                accountMaster.mobile_no = cell.stringCellValue
-//                            else if (index == 4)
-//                                accountMaster.opening_balance = cell.numericCellValue
-//                            else if (index == 5)
-//                                accountMaster.account_type = cell.stringCellValue
-//                            else if (index == 6)
-//                                accountMaster.party_type = cell.stringCellValue
-//                            else if (index == 7)
-//                                accountMaster.day = cell.stringCellValue
-//
-//                            index++
-//                        }
-//
-//                        /*when (cell.cellType) {
-//                            CellType.STRING -> {
-//                                Log.d("Excel Data", "String : ${cell.stringCellValue}")
-//                            }
-//                            CellType.NUMERIC -> {
-//                                Log.d("Excel Data", "Numeric : ${cell.numericCellValue}")
-//                            }
-//                            CellType._NONE -> TODO()
-//                            CellType.FORMULA -> Log.d("Excel Data", "FORMULA : ${cell.cellFormula}")
-//                            CellType.BLANK -> TODO()
-//                            CellType.BOOLEAN -> Log.d(
-//                                "Excel Data",
-//                                "BOOLEAN : ${cell.booleanCellValue}"
-//                            )
-//                            CellType.ERROR -> Log.d("Excel Data", "ERROR : ${cell.errorCellValue}")
-//                        }*/
-//
-//                    }
-//                    if (sharedPrefManager.getUserName == accountMaster.sales_man) {
-//                        aryAccount.add(accountMaster)
-//                        Log.e("readExcel", "readExcelAccountMasterFile: ${aryAccount.size}")
-//                    }
-//                }
-//            }
-//            workbook.close()
-//            inputSteam?.close()
-//
-//            return@withContext aryAccount
-//        }
-//    }
-//
-//    suspend fun readExcelItemMasterFile(uri: Uri): ArrayList<ItemMaster> {
-//        return withContext(Dispatchers.IO) {
-//            Log.d("Excel Data", "Item Master Start1")
-//
-//            var aryItems: ArrayList<ItemMaster> = ArrayList<ItemMaster>()
-//
-//            val inputSteam = contentResolver.openInputStream(uri)
-//            val workbook = XSSFWorkbook(inputSteam)
-//            //val workbook = WorkbookFactory.create(inputSteam)
-//            val sheet = workbook.getSheetAt(0)
-//            val iterator = sheet.iterator()
-//
-//            var isFirst: Boolean = true
-//            while (iterator.hasNext()) {
-//
-//                var itemMaster: ItemMaster = ItemMaster()
-//                val row = iterator.next()
-//                val cellIterator = row.cellIterator()
-//
-//                if (isFirst) {
-//                    isFirst = false
-//                } else {
-//
-//                    var index: Int = 0
-//                    while (cellIterator.hasNext()) {
-//                        val cell = cellIterator.next()
-//
-//                        if (cell.cellType != CellType.BLANK && cell.cellType != CellType.FORMULA) {
-//
-//                            if (index == 0)
-//                                itemMaster.category = cell.stringCellValue
-//                            else if (index == 1) {
-//                                itemMaster.company_name = cell.stringCellValue
-//                            } else if (index == 2)
-//                                itemMaster.brand = cell.stringCellValue
-//                            else if (index == 3) {
-//                                if (cell.cellType == CellType.STRING) {
-//                                    itemMaster.item_name = cell.stringCellValue
-//                                }
-//                            } else if (index == 4)
-//                                itemMaster.mrp = cell.numericCellValue
-//                            else if (index == 5)
-//                                itemMaster.purchase_rate = cell.numericCellValue
-//                            else if (index == 6)
-//                                itemMaster.sale_rate = cell.numericCellValue
-//                            else if (index == 7)
-//                                itemMaster.stock_qty = cell.numericCellValue
-//                            else if (index == 8)
-//                                itemMaster.stock_amount = cell.numericCellValue
-////                            else if (index == 9)
-////                                itemMaster.dump_day = cell.numericCellValue
-//
-//                            index++
-//                        }
-//
-//                        /*when (cell.cellType) {
-//                            CellType.STRING -> {
-//                                Log.d("Excel Data", "String : ${cell.stringCellValue}")
-//                            }
-//                            CellType.NUMERIC -> {
-//                                Log.d("Excel Data", "Numeric : ${cell.numericCellValue}")
-//                            }
-//                            CellType._NONE -> TODO()
-//                            CellType.FORMULA -> Log.d("Excel Data", "FORMULA : ${cell.cellFormula}")
-//                            CellType.BLANK -> TODO()
-//                            CellType.BOOLEAN -> Log.d(
-//                                "Excel Data",
-//                                "BOOLEAN : ${cell.booleanCellValue}"
-//                            )
-//                            CellType.ERROR -> Log.d("Excel Data", "ERROR : ${cell.errorCellValue}")
-//                        }*/
-//
-//                    }
-//                    aryItems.add(itemMaster)
-//                }
-//            }
-//            workbook.close()
-//            inputSteam?.close()
-//
-//            aryItems
-//        }
-//    }
-//
-
-//    private fun showAddExcelFileDialog(dialog: Dialog) {
-//
-//        dialog.setContentView(R.layout.dialog_add_excel_show)
-//        dialog.window?.setLayout(
-//            WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT
-//        )
-//        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-//        dialog.setCanceledOnTouchOutside(false)
-//        dialog.setCancelable(false)
-//        dialog.show()
-//
-//        dialog.findViewById<TextView>(R.id.btnAccountExcel).setOnClickListener {
-//            fileType = 1
-//            if (checkPermission()) {
-//                openFilePicker()
-//            } else {
-//                requestPermission()
-//            }
-//        }
-//        dialog.findViewById<TextView>(R.id.btnItemExcel).setOnClickListener {
-//            fileType = 2
-//            if (checkPermission()) {
-//                openFilePicker()
-//            } else {
-//                requestPermission()
-//            }
-//        }
-//
-//    }
-
-    fun setProgressShowDialog(context: Activity,string: String) {
+    fun setProgressShowDialog(context: Activity, string: String) {
         showingDialog = ShowingDialog(context, string)
         showingDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         showingDialog.setCanceledOnTouchOutside(false)
         showingDialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
     }
+
     fun showDialog(context: Activity) {
         if (!context.isFinishing) {
             showingDialog.show()
--- app/src/main/java/com/sunil/dhwarehouse/adapter/AccountDataAdapter.kt
+++ app/src/main/java/com/sunil/dhwarehouse/adapter/AccountDataAdapter.kt
@@ -3,11 +3,10 @@ package com.sunil.dhwarehouse.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.Recycler
 import com.sunil.dhwarehouse.RoomDB.AccountMaster
 import com.sunil.dhwarehouse.databinding.AccountItemRowBinding
 
-class AccountDataAdapter(private val userList: MutableList<AccountMaster>) :
+class AccountDataAdapter(private val masterMutableList: MutableList<AccountMaster>) :
     RecyclerView.Adapter<AccountDataAdapter.MyAccountHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAccountHolder {
@@ -17,10 +16,10 @@ class AccountDataAdapter(private val userList: MutableList<AccountMaster>) :
         return MyAccountHolder(binding)
     }
 
-    override fun getItemCount(): Int = userList.size
+    override fun getItemCount(): Int = masterMutableList.size
 
     override fun onBindViewHolder(holder: MyAccountHolder, position: Int) {
-        holder.bind(userList[position])
+        holder.bind(masterMutableList[position])
     }
 
     class MyAccountHolder(private val binding: AccountItemRowBinding) :
